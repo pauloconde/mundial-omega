78df489f471cc2aebe277b8984ccbd9e5e0771d7
Paulo Conde <pauloconde@gmail.com>
2026-05-12 21:23:59 -0400

    fix: ogimage deploy url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sponsorInfo from './src/data/sponsor.json';
 
 // https://astro.build/config
 export default defineConfig({
+  site: sponsorInfo.deployUrl,
   vite: {
     plugins: [tailwindcss()]
   },
